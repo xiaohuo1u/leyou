1dec81e2f77f958251c48665b610fcfd0eb9e606
Your Name <you@example.com>
2020-12-07 11:06:59 +0800

    Initial commit

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -0,0 +1,10 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+
+public class LyItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemApplication.class,args);
+    }
+
+}
